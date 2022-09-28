97cbf8facaf1c643db7bc501fa38cac104fb45e4
Angiie <57513420+Maurenanny@users.noreply.github.com>
2022-09-27 22:19:07 -0500

    Update sw

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 self.addEventListener('install', (event) => {
     console.log('SW: install');
-    const cachePromise = caches.open('cache-vSW1').then((cache) => {
+    const cachePromise = caches.open('cache-v1').then((cache) => {
         return cache.addAll(
             [
-                '/',
-                '/index.html',
-                '/css/style.css',
-                '/js/app.js',
-                '/pages/dividir.html',
-                '/pages/sumar.html',
-                '/pages/restar.html',
-                '/pages/multiplicar.html',
+                './',
+                './index.html',
+                './css/style.css',
+                './js/app.js',
+                './pages/dividir.html',
+                './pages/sumar.html',
+                './pages/restar.html',
+                './pages/multiplicar.html',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js'
             ]
@@ -20,6 +20,7 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    const cacheRes = caches.match(event.request)
+    console.log(event.request.url);
+    const cacheRes = caches.match(event.request);
     event.respondWith(cacheRes);
-})
+});
